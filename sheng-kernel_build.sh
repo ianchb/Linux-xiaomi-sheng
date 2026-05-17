a65e70d5dd53a930a539af3253921836d996f251
ianchb <i@4t.pw>
2026-05-17 13:18:25 +0800

    switch to forked kernel

--- sheng-kernel_build.sh
+++ sheng-kernel_build.sh
@@ -18,7 +18,7 @@ export OBJDUMP="llvm-objdump"
 export READELF="llvm-readelf"
 export STRIP="llvm-strip"
 
-git clone https://github.com/map220v/sm8550-mainline.git --branch sheng-7.0 --depth 1 linux
+git clone https://github.com/ianchb/sm8550-mainline.git --branch sheng-7.0 --depth 1 linux
 cd linux
 
 wget https://gitlab.postmarketos.org/alghiffaryfa19/pmaports/-/raw/sheng/device/testing/linux-postmarketos-qcom-sm8550/config-postmarketos-qcom-sm8550.aarch64 -O .config
